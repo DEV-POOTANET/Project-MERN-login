c7b094e0a1fdfc81f85f88745b1cb4f0fa5fe591
DEV-POOTANET <pootanetrampuey@gmail.com>
2024-04-12 01:55:38 +0700

    add model user and api register

--- server/Controller/auth.js
+++ server/Controller/auth.js
@@ -1,7 +1,25 @@
+const bcrypt = require('bcryptjs')
+const User   = require('../model/User')
 
 exports.register = async(req,res)=>{
-    try{        
-        res.send(req.body.username)
+    try{     
+        // cheak user
+        const { username , password } = req.body;
+        var user = await User.findOne({username})
+            //console.log(user)
+        if(user){
+           return res.status(400).send("User Already exists");
+        }
+        const salt = await bcrypt.genSalt(10)
+        user = new User({
+            username,
+            password,
+        });
+        // Encrypt   
+        user.password = await bcrypt.hash(password,salt);
+        await user.save();
+        res.send("Register Success")
+
     }catch(err){
         console.log(err)
         res.status(500).send('server error !')
