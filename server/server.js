a8573ec7ce16dc59622d349cae8d2a66ba5b604c
DEV-POOTANET <pootanetrampuey@gmail.com>
2024-04-12 01:13:46 +0700

    connnect mongoDB

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express =         require('express')
 const cors =            require('cors')
 const bodyParser =      require('body-parser')
 const morgan =          require('morgan')
-const dotenv =          require('dotenv')
 const { readdirSync } = require('fs')
+require('dotenv').config();
+const connectDB =       require('./config/db')
 
 const app = express();
-const port = '9090'
+
+// connectDB
+connectDB()
 
 //middleware
 app.use(morgan('dev'))
@@ -16,13 +19,14 @@ app.use(cors())
 
 // Route
 //#1
-app.use('/api',require('./routes/api'))
+//app.use('/api',require('./routes/api'))
 
 // Route
 //#2 (auto)
 readdirSync('./routes')
 .map((r) => app.use('/api', require('./routes/'+r)))
 
+const port = process.env.PORT
 app.listen(port,()=>{
     console.log('server in running on port'+ port)
 });
