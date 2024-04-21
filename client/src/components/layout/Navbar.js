348e93f95ed1c6387a1f829f62b7741b3adfcc0d
DEV-POOTANET <pootanetrampuey@gmail.com>
2024-04-21 22:53:54 +0700

    Login + navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LoginOutlined,  } from '@ant-design/icons';
+import { Menu } from 'antd';
+
+import { Link } from 'react-router-dom'
+const items = [
+    {
+      label: 'Login',
+      key: 'login',
+      icon: <LoginOutlined />,
+      to: '/login'
+    },
+    {
+      label: 'Register',
+      key: 'register',
+    //   icon: <AppstoreOutlined />,
+      to: '/register'
+    },
+  ];
 
 const Navbar = () => {
-  return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  );
-}
+    const [current, setCurrent] = useState('login');
+    const onClick = (e) => {
+      console.log('click ', e);
+      setCurrent(e.key);
+    };
 
+    return    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
+                {items.map(item => (
+                <Menu.Item key={item.key} icon={item.icon}>
+                    <Link to={item.to}>{item.label}</Link>
+                </Menu.Item>
+                ))}
+            </Menu>
+};
 export default Navbar;
--- client/src/components/page/auth/Login.js
+++ client/src/components/page/auth/Login.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React , { useState } from 'react';
+
+//function
+import { login } from '../../function/auth';
+
+
 
 function Login() {
+  const [ value , setValue] = useState({
+    username : "",
+    password : ""
+  })
+  
+  const handleChange = (e) =>{
+          
+    setValue({
+      ...value,
+      [e.target.name] : e.target.value,
+  })
+  }
+  
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+    console.log(value)
+      login(value)
+      .then(res =>{
+        console.log(res)
+        alert(res.data)
+      }).catch(err =>{
+        console.log(err.response.data)
+        alert(err.response.data)
+      })
+  }
+
   return (
     <div>
-      <h1>login page</h1>
-    </div>
+    <h1>Login</h1>
+    <form onSubmit={handleSubmit}>
+
+      <label>Username</label>
+      <input type='text' name='username' onChange={handleChange} />
+
+      <label>Password</label>
+      <input type='text' name='password' onChange={handleChange} />     
+
+      <button type='submit' disabled={value.password.length < 6  } >Submit</button>
+
+    </form>
+  </div>
   );
 }
 
